41fe2e72237e42f06f24da694231b26aaffa5b56
ArshhGill <arshh.gill@outlook.com>
2024-04-07 15:36:59 +0530

    get data from db

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-echo "uwu";
-?>
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "phpclg0424";
+$dbconn = new mysqli($servername, $username, $password, $dbname);
+$id = 'eid';
+$title = 'etitle';
+$date = 'edatetime';
+
+if ($dbconn->connect_error) {
+    die("Connection failed: " . $dbconn->connect_error);
+}
+
+$result = $dbconn->query("select * from calendar");
+while ($row = $result->fetch_assoc()) {
+    echo eventDiv($row[$id], $row[$title], explode(" ", $row[$date])[0]);
+}
+
+function eventDiv($id, $title, $date): string
+{
+    return "<div class='eventDiv' id='$id'>
+    <span>$title</span>
+    </span>$date</span>
+    </div>";
+}
