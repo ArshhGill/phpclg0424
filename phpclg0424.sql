0f53ffb0b013956d3dcaf5bef09ea5bc962c10ed
ArshhGill <arshh.gill@outlook.com>
2024-04-14 23:47:22 +0530

    noitall loves githhi so much its unreal bruh

---
+++ phpclg0424.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `uname` VARCHAR(16)                          PRIMARY KEY,
+    `upass` VARCHAR(255)                         NOT NULL,
+    `utype` SET ('user', 'admin') DEFAULT 'user' NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `calendar`
+(
+    `cid`   INT(11) AUTO_INCREMENT PRIMARY KEY,
+    `cname` VARCHAR(255) NOT NULL,
+    `uname` VARCHAR(16)  NOT NULL,
+    FOREIGN KEY (`uname`) REFERENCES `users` (`uname`)
+);
+
+CREATE TABLE IF NOT EXISTS `events`
+(
+    `eid`    INT(8)       AUTO_INCREMENT PRIMARY KEY,
+    `etitle` VARCHAR(255) NOT NULL,
+    `edate`  DATE         NOT NULL,
+    `cid`    INT(11)      NOT NULL,
+    FOREIGN KEY (`cid`) REFERENCES `calendar` (`cid`)
+);
